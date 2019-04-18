2b45c730b84cd82cf72cc959336b974ce99c1c38
Alexander Mandera <alexander@mandera.eu>
2019-04-19 00:27:39 +0200

    Add angular project with routing and login, register, password reset pages

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
+import {BoxComponent} from "./box/box.component";
+import {HomeComponent} from "./box/home/home.component";
+import {FilesComponent} from "./box/files/files.component";
+import {AppsComponent} from "./box/apps/apps.component";
+import {SettingsComponent} from "./box/settings/settings.component";
+import {PasswordResetComponent} from "./password-reset/password-reset.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'login', component: LoginComponent },
+  { path: 'password-reset', component: PasswordResetComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'box', component: BoxComponent, children: [
+      { path: '', pathMatch: 'full', redirectTo: 'home' },
+      { path: 'home', component: HomeComponent },
+      { path: 'files', component: FilesComponent },
+      { path: 'apps', component: AppsComponent },
+      { path: 'settings', component: SettingsComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { BoxComponent } from './box/box.component';
+import {AppsComponent} from "./box/apps/apps.component";
+import {FilesComponent} from "./box/files/files.component";
+import {HomeComponent} from "./box/home/home.component";
+import {SettingsComponent} from "./box/settings/settings.component";
+import { PasswordResetComponent } from './password-reset/password-reset.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    BoxComponent,
+    AppsComponent,
+    FilesComponent,
+    HomeComponent,
+    SettingsComponent,
+    PasswordResetComponent
   ],
   imports: [
     BrowserModule,
